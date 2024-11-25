1c93268d6f622afa3aef2f9500af2a4f30322df1
lonakhemmoro <124197218+lonakhemmoro@users.noreply.github.com>
2024-11-24 23:32:22 -0500

    work on main activity

--- app/src/main/java/com/example/habithero/MainActivity.java
+++ app/src/main/java/com/example/habithero/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.habithero;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,49 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button trackBtn;
+    Button historyBtn;
+    Button focusTimerBtn;
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        trackBtn = findViewById(R.id.trackBtn);
+        historyBtn = findViewById(R.id.historyBtn);
+        focusTimerBtn = findViewById(R.id.focusTimerBtn);
+
+        trackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent=new Intent(view.getContext(), HabitActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        focusTimerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent=new Intent(view.getContext(), TimerActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
         });
     }
 }
